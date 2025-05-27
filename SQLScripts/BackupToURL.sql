372d5caaa2f1309cb577321b522f21f75904f4af
ilikesql <dandy.weyn@microsoft.com>
2025-05-27 04:19:04 -0700

    removed script

--- SQLScripts/BackupToURL.sql
+++ SQLScripts/BackupToURL.sql
@@ -2,7 +2,7 @@
 -- create a storage credential
 CREATE CREDENTIAL [https://ilikesqlmigrationblob.blob.core.windows.net/sqlbackups]
 WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-SECRET = 'sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-05-31T17:48:28Z&st=2025-05-27T09:48:28Z&spr=https&sig=Ho%2FvGbTzTIYfKyGhXgDU0XTN41HHXGzicgekZ0H8i1c%3D'
+SECRET = ''
 
 
 BACKUP DATABASE Northwind
